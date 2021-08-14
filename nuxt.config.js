6054075a1cb0bcf208b21f4a2bef1b7bc15452a6
Marina Potovichenko <meowdy@mail.ru>
2021-08-14 19:01:25 +0300

    feat: project setup (#1)
    Co-authored-by: Marina Potovichenko <mpotovichenko@ozon.ru>

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,53 +1,41 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'nuxt-playground',
-    htmlAttrs: {
-      lang: 'en'
+    target: 'static',
+    mode: 'spa',
+    head: {
+        title: 'nuxt-playground',
+        htmlAttrs: {
+            lang: 'en',
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+    loading: {
+        color: 'red',
+    },
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+    css: [{ src: '~/css/main.css', lang: 'postcss' }],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    plugins: [],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module',
-  ],
+    build: {
+        postcss: {
+            plugins: {
+                'postcss-nested': {},
+            },
+        },
+    },
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
+    components: true,
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+    buildModules: ['@nuxt/typescript-build', '@nuxtjs/stylelint-module'],
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+    modules: ['@nuxtjs/axios'],
+
+    axios: {},
+};
