9f6a94a197734e1dd297eb1a0a38d6d0cefd111f
Marina Potovichenko <mpotovichenko@ozon.ru>
2021-08-15 21:41:54 +0300

    feat: base tree layout

--- components/ui/CustomTree/Tree.vue
+++ components/ui/CustomTree/Tree.vue
@@ -1,11 +1,48 @@
 <template>
-    <div></div>
+    <ul>
+        <li>
+            <div class="blue--text">
+                {{node.data.title}}
+            </div>
+
+            <ol v-if="node.data.users">
+                <template v-if="node.data.users.lead">
+                    <li class="red--text">
+                        {{node.data.users.lead.name}}
+                    </li>
+                </template>
+                <template v-if="node.data.users.employees">
+                    <li
+                        v-for="user in node.data.users.employees"
+                        :key="user.name"
+                    >
+                        {{user.name}}
+                    </li>
+                </template>
+
+            </ol>
+
+            <template v-if="node.child">
+                <template v-for="child in node.child">
+                    <Node
+                        :node="child"
+                        :key="child.data.title"
+                    />
+                </template>
+            </template>
+        </li>
+    </ul>
 </template>
 
 <script lang='ts'>
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Prop, Vue } from 'nuxt-property-decorator';
+import { TreeNode } from '~/types/Tree.ts';
 
-@Component({})
-export default class Tree extends Vue {}
+@Component({
+    name: 'Node',
+})
+export default class TreeLayout extends Vue {
+    @Prop({type: [TreeNode, null], default: null}) node!: TreeNode | null;
+}
 </script>
 
