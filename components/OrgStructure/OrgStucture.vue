c5d57ff84d68f2a12f9c3d1bb2ab615e3ef57a22
Marina Potovichenko <meowdy@mail.ru>
2022-06-11 20:06:06 +0300

    Feature/users-tree (#3)
    * feat: created tree structure, mock
    
    * feat: base tree layout
    
    * feat: changes typing and added mock script
    
    * fix: fixed typing
    
    * feat: added postcss config
    
    Co-authored-by: Marina Potovichenko <mpotovichenko@ozon.ru>

---
+++ components/OrgStructure/OrgStucture.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Tree :node="tree.root" />
+    </div>
+</template>
+
+<script lang='ts'>
+import { Component, Prop, Vue } from 'nuxt-property-decorator';
+import { Tree } from '~/types/Tree.ts';
+
+@Component({
+    components: {
+        Tree: () => import('~/components/ui/CustomTree/Tree.vue'),
+    },
+})
+export default class OrgStucture extends Vue {
+    @Prop({type: [Tree, null], default: null}) tree!: Tree | null;
+}
+</script>
